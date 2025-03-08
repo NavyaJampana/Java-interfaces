cba69031ab0885e12f8d421c2fc9e3b2e7e8eaa0
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 17:21:57 +0530

    Add files via upload

---
+++ StaticFinalVariable.java
@@ -0,0 +1,11 @@
+package Interfaces;
+interface StaticFinalInterface {
+    static final int CONSTANT = 42;
+}
+
+class StaticFinalClass implements StaticFinalInterface {
+    public static void main(String[] args) {
+        System.out.println("Constant value: " + CONSTANT);
+    }
+}
+
